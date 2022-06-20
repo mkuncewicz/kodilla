3a0ad0d97d4bb3e470371f57d9852ae289ad511a
mkuncewicz <mateusz.kuncewicz@onet.pl>
2022-06-20 02:34:37 +0200

    6.2 Calculator class kodilla-testing module

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,24 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if(result.equals("theForumUser")){
+            System.out.println("OK!");
+        }else {
+            System.out.println("Error!");
+        }
+
+
+        System.out.println("Dodawanie 5+2= " + Calculator.add(5,2));
+        System.out.println("Odejmowanie 16-31= " + Calculator.subtract(16,31));
+
     }
 }
