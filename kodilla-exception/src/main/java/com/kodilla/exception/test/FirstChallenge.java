43719cd9a97be5c788c85e83fb31cfd68684630c
mkuncewicz <mateusz.kuncewicz@onet.pl>
2022-07-10 07:00:04 +0200

    8.2 Module - Divice tryCatch

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -0,0 +1,24 @@
+package com.kodilla.exception.test;
+
+public class FirstChallenge {
+
+    public double divide(double a, double b) throws ArithmeticException {
+        try {
+            if (b == 0) {
+                throw new ArithmeticException();
+            }
+            return a / b;
+        }catch (ArithmeticException e){
+            System.out.println("Nie dzieli sie przez zero!!" + e);
+            return 0;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        FirstChallenge firstChallenge = new FirstChallenge();
+        double result = firstChallenge.divide(3, 0);
+
+        System.out.println(result);
+    }
+}
