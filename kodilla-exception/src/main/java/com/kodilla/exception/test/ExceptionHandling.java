971b1fb73cf0554ea16f9ba305e201b044113fc5
mkuncewicz <mateusz.kuncewicz@onet.pl>
2022-07-11 11:41:25 +0200

    8.3 Module

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -1,15 +1,5 @@
 package com.kodilla.exception.test;
 
-public class ExceptionHandling {
+public class ExceptionHandling extends Exception {
 
-
-    public void secondChallenge() {
-        SecondChallenge secondChallenge = new SecondChallenge();
-        try {
-            String string = secondChallenge.probablyIWillThrowException(1.5, 4);
-            System.out.println(string);
-        } catch (Exception e) {
-            System.out.println("BLAD BQ !: " + e);
-        }
-    }
 }
--- kodilla-exception/src/test/java/com/kodilla/exception/io/SecondChallengeTestSuite.java
+++ kodilla-exception/src/test/java/com/kodilla/exception/io/SecondChallengeTestSuite.java
@@ -11,9 +11,14 @@ public class SecondChallengeTestSuite {
     @Test
     void probablyIWillThrowExceptionTest(){
         //Given
-        ExceptionHandling exceptionHandling = new ExceptionHandling();
+        SecondChallenge secondChallenge = new SecondChallenge();
         //When && Then
-
+        assertAll(
+                () -> assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(3,1)),
+                () -> assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(0,1)),
+                () -> assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(1,1.5)),
+                () -> assertDoesNotThrow(() -> secondChallenge.probablyIWillThrowException(1,1))
+        );
 
 
     }
