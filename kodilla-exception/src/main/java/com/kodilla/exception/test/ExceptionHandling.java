1f2c509c9a6349ef87f506843359f3dbf3f7ecf2
mkuncewicz <mateusz.kuncewicz@onet.pl>
2022-07-12 09:08:29 +0200

    8.3 Module - Correct

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -1,5 +1,17 @@
 package com.kodilla.exception.test;
 
-public class ExceptionHandling extends Exception {
+public class ExceptionHandling {
+    public static void main(String[] args) {
+
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        try {
+            String string = secondChallenge.probablyIWillThrowException(1.5,4);
+            System.out.println(string);
+        }catch (Exception e){
+            System.out.println("BLAD! " + e);
+        }
+    }
+
 
 }
